6cec0ec0c69e9feaa8d7965abe82d1f254b860ac
Akshay <akshayy976@gmail.com>
2021-12-23 12:39:42 +0530

    exercise

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,15 +1,25 @@
-// const fs = require('fs')
+const fs = require('fs')
+const chal = require('chalk')
+const validate = require('validator')
 
 // fs.writeFileSync('demo.txt', 'this is 1 demo file')
-
 // fs.appendFileSync('demo.txt', 'this is 2 demo file')
 
 const utils = require('./utils')
-console.log('in app');
-console.log(utils.add(4, 2));
+    // console.log('in app');
+    // console.log(utils.add(4, 2));
 
 
-// exercise
+// 10 exercise
 const notes = require('./notes')
+const { green } = require('color-name')
 const note = notes.getNotes()
-console.log(note);
+    // console.log(note);
+
+
+// 11.
+console.log(validate.isEmail('ak@gm.com'));
+console.log(validate.isURL('http://ak.com'));
+
+// 12. exercise
+console.log(chal.green.bold('Success!'));
