2393cf753e315f6e87d6ddbebed36b0ba7772ce2
Akshay <akshayy976@gmail.com>
2021-12-23 15:39:31 +0530

    add title

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 const chal = require('chalk')
 const validate = require('validator')
-
-// fs.writeFileSync('demo.txt', 'this is 1 demo file')
-// fs.appendFileSync('demo.txt', 'this is 2 demo file')
+const yargs = require('yargs')
+    // fs.writeFileSync('demo.txt', 'this is 1 demo file')
+    // fs.appendFileSync('demo.txt', 'this is 2 demo file')
 
 const utils = require('./utils')
     // console.log('in app');
@@ -22,4 +22,66 @@ console.log(validate.isEmail('ak@gm.com'));
 console.log(validate.isURL('http://ak.com'));
 
 // 12. exercise
-console.log(chal.green.bold('Success!'));
+console.log(chal.green.bold('Success!!'));
+
+// 15. input from user
+console.log(process.argv[2]);
+
+// const command = process.argv[2]
+// if (command === 'add') {
+//     console.log('Adding Note');
+// } else if (command === 'remove') {
+//     console.log('Removing Note');
+// }
+
+// console.log(process.argv);
+
+// creating add note command
+yargs.command({
+    command: 'add',
+    describe: 'Add new note',
+    builder: {
+        title: {
+            describe: "note title",
+            demandOption: true
+        }
+
+    },
+    handler: function(argv) {
+        console.log('Adding new note', argv);
+    }
+})
+
+
+// remove note command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove note',
+    handler: function() {
+        console.log('Remoing note');
+    }
+})
+
+
+
+// list note command
+yargs.command({
+    command: 'list',
+    describe: 'list note',
+    handler: function() {
+        console.log('Listing note');
+    }
+})
+
+
+
+// read note command
+yargs.command({
+    command: 'read',
+    describe: 'read note',
+    handler: function() {
+        console.log('Reading note');
+    }
+})
+
+console.log(yargs.argv);
