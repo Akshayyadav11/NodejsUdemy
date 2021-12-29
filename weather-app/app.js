c26e84096fa110007554a8a176a5503bc98c215e
Akshay <akshayy976@gmail.com>
2021-12-29 15:45:47 +0530

    http

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,19 @@
-console.log('Starting');
+// console.log('Starting');
 
-setTimeout(() => {
-    console.log('2 second timer');
-}, 2000);
+// setTimeout(() => {
+//     console.log('2 second timer');
+// }, 2000);
 
-setTimeout(() => {
-    console.log('0 second timer');
-}, 0);
-console.log('Stopping');
+// setTimeout(() => {
+//     console.log('0 second timer');
+// }, 0);
+// console.log('Stopping');
+
+// 31. HTTP req
+const request = require('request')
+const url = 'http://api.weatherstack.com/current?access_key=38dc4ca299daf7cd4ff3897bca457381&query=Mumbai'
+request({ url: url, json: true }, (err, resp) => {
+
+    // const data = JSON.parse(resp.body)  as we have used json=true
+    console.log(resp.body.current);
+})
