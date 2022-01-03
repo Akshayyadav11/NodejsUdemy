25784e8a7a20196cd8767cfef6a84c83c6b7923d
Akshay <akshayy976@gmail.com>
2022-01-03 16:38:05 +0530

    dir

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,28 +1,42 @@
 const express = require('express')
+const path = require('path')
+
+
+// console.log(__dirname);
+// console.log(path.join(__dirname, '../public'));
+
 const app = express()
 
-app.get('', (req, res) => {
-    res.send('Hello express')
-})
+const publicDirPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirPath))
 
 
-app.get('/help', (req, res) => {
-    res.send(' Hello help page')
-})
+// removing abt, hlp as reading from public folder
+// app.get('/about', (req, res) => {
+//     res.send('<html><title>About</title><body>This is about page</body></html>')
+// })
 
 
-app.get('/about', (req, res) => {
-    res.send('<html><title>About</title><body>This is about page</body></html>')
-})
+// app.get('/help', (req, res) => {
+//     res.send([{
+//             name: 'akshay',
+//             age: 30
+//         },
+//         {
+//             name: 'yadav',
+//             age: 30
+//         }
+//     ])
+// })
 
 
 app.get('/weather', (req, res) => {
-    res.send('<html><title>Weather</title><body>This is weather page</body></html>')
-})
-
-
-app.get('/help', (req, res) => {
-    res.send(' Hello help page')
+    // res.send('<html><title>Weather</title><body>This is weather page</body></html>')
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Mumbai'
+    })
 })
 
 
